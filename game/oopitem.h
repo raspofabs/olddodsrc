c780ca2fb038344b862d84e03757bc21ad1a7d31
Richard Fabian <raspo1@gmail.com>
2014-01-27 00:37:29 +0000

    adding the oopitem header

---
+++ game/oopitem.h
@@ -0,0 +1,17 @@
+#ifndef _OOP_ITEM_H_
+#define _OOP_ITEM_H_
+
+class Item {
+	public:
+		Item( int type, int cost, bool isUnique ) : m_Type( type ), m_Cost( cost ), m_IsUnique( isUnique ) {}
+		bool GetType() { return m_Type; }
+		int GetCost() { return m_Cost; }
+		int IsUnique() { return m_IsUnique; }
+
+	private:
+		int m_Type;
+		int m_Cost;
+		bool m_IsUnique;
+};
+
+#endif
